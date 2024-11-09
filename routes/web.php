ddf0f7bc65b6a439b35e500664d8caacb8841acc
unknown <SENAI@C303-LAB05-26.senaiescola.fsa>
2024-11-09 11:42:38 -0300

    novo

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LocationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::post('/get_location', [LocationController::class, 'getLocation'])->name('get_location');
